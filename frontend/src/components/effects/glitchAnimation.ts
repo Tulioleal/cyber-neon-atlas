266ab3dc24d0b5787c2c481055238b62365df8c8
Tulioleal <tuliolealdev@gmail.com>
2026-04-10 18:53:42 -0300

    fix: initial state

--- frontend/src/components/effects/glitchAnimation.ts
+++ frontend/src/components/effects/glitchAnimation.ts
@@ -4,16 +4,6 @@ import { HTMLMotionProps } from "motion/react"
 type AnimationProps = HTMLMotionProps<"div">
 
 const containerAnimationProps = (isPrimary: boolean = true):AnimationProps => ({
-  initial: { backgroundColor: colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0) },
-  animate: {
-    backgroundColor: [
-      colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0),     // Start transparent
-      colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0.5),   // Flash bright
-      colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0.1),   // Settle
-      colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0.2),   // Glitch
-      colorsWithAlpha[isPrimary ? 'primary' : 'secondary'](0),     // End transparent
-    ]
-  },
   exit:{
     backgroundColor: [
       colorsWithAlpha[ isPrimary ? 'primary' : 'secondary' ](0.1),
